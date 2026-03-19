9971dc76c17308280fa0976dffa1a5aa04398cc5
Jems Staleen <jemsstaleen5@gmail.com>
2026-03-19 16:22:37 +0530

    Implemented retry logic for DB connection

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import os
 import psycopg2
+import time
 
 app = Flask(__name__)
 
@@ -8,6 +9,23 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "testdb")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASS = os.getenv("DB_PASS", "password")
+
+def connect_with_retry():
+    retries = 5
+    while retries > 0:
+        try:
+            conn = psycopg2.connect(
+                host=DB_HOST,
+                database=DB_NAME,
+                user=DB_USER,
+                password=DB_PASS
+            )
+            return conn
+        except Exception as e:
+            print(f"DB not ready, retrying... {e}")
+            retries -= 1
+            time.sleep(5)
+    raise Exception("Database not reachable")
 
 @app.route("/")
 def home():
@@ -20,12 +38,7 @@ def health():
 @app.route("/db")
 def db_test():
     try:
-        conn = psycopg2.connect(
-            host=DB_HOST,
-            database=DB_NAME,
-            user=DB_USER,
-            password=DB_PASS
-        )
+        conn = connect_with_retry()
         conn.close()
         return {"database": "connected"}
     except Exception as e:
