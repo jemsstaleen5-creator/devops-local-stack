1076db1e7d2555ff957ba1aba4fef0595ea81e96
Jems Staleen <jemsstaleen5@gmail.com>
2026-03-25 14:40:18 +0530

    Added deployment script and CD simulation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,9 @@ fi
 
 echo "Deploying image: $IMAGE_NAME"
 
+#Update image
 kubectl set image deployment/flask-app flask-app=$IMAGE_NAME
-
+#Wait for rollout
 kubectl rollout status deployment flask-app
 
 echo "Deployment Successful 🚀"
